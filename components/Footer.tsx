c0e9fba19c8c8dc945c0f4928390ec14f3519435
Abyssifex <borgesoiiricardo@gmail.com>
2026-04-18 22:30:18 -0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
   return (
     <footer className="py-6 text-gray-500 text-sm">
-      <div className="text-center">© {new Date().getFullYear()} João Silva — Full-Stack & Security</div>
+      <div className="text-center">© {new Date().getFullYear()} Abyssifex — Full-Stack & Security</div>
     </footer>
   )
 }
